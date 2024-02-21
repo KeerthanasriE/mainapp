999f4d4b13d0d90c1ae68532e77b9cf8bf70392e
KeerthanasriE <keerthana21egmail.com>
2024-02-21 21:46:59 +0530

    updated

---
+++ caluculator.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def divi(x,y):
+    if y==0:
+        print("invalid")
+    else:
+        return x/y
+subration=sub(2,3)
+print(subration)
+division=divi(9,0)
+print(division)
